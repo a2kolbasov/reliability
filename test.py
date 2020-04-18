7964eb4d6bb696c3f13a566bcd97743161d16eeb
Aleksandr Kolbasov <kolbasov.a.a@edu.mirea.ru>
2020-04-18 20:20:33 +0300

    Новые тесты

--- test.py
+++ test.py
@@ -6,17 +6,25 @@ nodes = Node.list(0.99, 0.8, 0.8, 0.8)
 
 for node in nodes:
     print(node)
-print()
+
+print(
+    """
+             /---node_1---\\
+            |              |
+---node_0---+----node_2----+---
+            |              |
+             \\---node_3---/
+    """
+)
 
 print(
     nodes[0] & (nodes[1] | nodes[2] | nodes[3])
-    # nodes[0] * (nodes[1] + nodes[2] + nodes[3])
-
-    #               /---node_1---\
-    #              |              |
-    #  ---node_0---+----node_2----+---
-    #              |              |
-    #               \---node_3---/
+)
+print(
+    nodes[0] * (nodes[1] + nodes[2] + nodes[3])
+)
+print(
+    Node.serial(0.99, Node.parallel(0.8, 0.8, 0.8))
 )
 
 print('\n################\n')
@@ -27,3 +35,10 @@ print("{}\nСервер 2: {}\n".format(a1, a2))
 
 print("Последовательно: ", a1 & a2)
 print("Параллельно: ", a1 | a2)
+
+print("Последовательно: {}".format(
+    Node.serial(0.99, 0.6)
+))
+print("Параллельно: {}".format(
+    Node.parallel(a1.p, a2.p)
+))
